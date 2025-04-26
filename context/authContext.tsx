45d9311f86789f421b7fba4c34083ea3630a7d5d
preston176 <prestonnyamweya@gmail.com>
2025-04-26 20:34:10 +0300

    feat: implement authentication context with login and registration functionality

---
+++ context/authContext.tsx
@@ -0,0 +1,67 @@
+import { auth, firestore } from "@/config/firebase";
+import { AuthContextType, UserType } from "@/types";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { setDoc, doc, getDocs, getDoc } from "firebase/firestore";
+import React, { createContext, useState, useEffect } from "react";
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<UserType | null>(null);
+
+  const login = async (email: string, password: string) => {
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      await updateUserData(res.user.uid);
+      return { success: true };
+    } catch (error: any) {
+      const msg = error?.message;
+      return { success: false, msg };
+    }
+  };
+
+  const register = async (email: string, password: string, name: string) => {
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      await setDoc(doc(firestore, "users", res.user.uid), {
+        name,
+        email,
+        uid: res.user.uid,
+        image: null, // or empty if you like
+      });
+
+      await updateUserData(res.user.uid);
+      return { success: true };
+    } catch (error: any) {
+      const msg = error?.message;
+      return { success: false, msg };
+    }
+  };
+
+  const updateUserData = async (uid: string) => {
+    try {
+      const docRef = doc(firestore, "users", uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+
+        const userData: UserType = {
+          uid: data.uid,
+          email: data.email || null,
+          name: data.name || null,
+          image: data.image || null,
+        };
+        setUser(userData);
+      }
+    } catch (error) {
+      console.log("Error updating user data:", error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, register, setUser, updateUserData }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
