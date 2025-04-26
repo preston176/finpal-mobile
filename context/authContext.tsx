7159257c806c63c0312bb0e7b4149ceece647715
preston176 <prestonnyamweya@gmail.com>
2025-04-26 21:17:17 +0300

    feat: refactor AuthProvider to include onAuthStateChanged for user state management

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -1,81 +1,93 @@
 import { auth, firestore } from "@/config/firebase";
 import { AuthContextType, UserType } from "@/types";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { setDoc, doc, getDocs, getDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { setDoc, doc, getDoc } from "firebase/firestore";
 import React, { createContext, useState, useEffect, useContext } from "react";
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [user, setUser] = useState<UserType | null>(null);
+    const [user, setUser] = useState<UserType | null>(null);
 
-  const login = async (email: string, password: string) => {
-    try {
-      const res = await signInWithEmailAndPassword(auth, email, password);
-      await updateUserData(res.user.uid);
-      return { success: true };
-    } catch (error: any) {
-      const msg = error?.message;
-      return { success: false, msg };
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, async (firebaseUser) => {
+          if (firebaseUser) {
+            await updateUserData(firebaseUser.uid);
+          } else {
+            setUser(null);
+          }
+        });
+      
+        return unsub;
+      }, []);
+
+    const login = async (email: string, password: string) => {
+        try {
+            const res = await signInWithEmailAndPassword(auth, email, password);
+            await updateUserData(res.user.uid);
+            return { success: true };
+        } catch (error: any) {
+            const msg = error?.message;
+            return { success: false, msg };
+        }
+    };
+
+    const register = async (email: string, password: string, name: string) => {
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+
+            await setDoc(doc(firestore, "users", res.user.uid), {
+                name,
+                email,
+                uid: res.user.uid,
+                image: null, // or empty if you like
+            });
+
+            await updateUserData(res.user.uid);
+            return { success: true };
+        } catch (error: any) {
+            const msg = error?.message;
+            return { success: false, msg };
+        }
+    };
+
+    const updateUserData = async (uid: string) => {
+        try {
+            const docRef = doc(firestore, "users", uid);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                const data = docSnap.data();
+
+                const userData: UserType = {
+                    uid: data.uid,
+                    email: data.email || null,
+                    name: data.name || null,
+                    image: data.image || null,
+                };
+                setUser(userData);
+            }
+        } catch (error) {
+            console.log("Error updating user data:", error);
+        }
+    };
+
+    const contextValue: AuthContextType = {
+        user,
+        setUser,
+        login,
+        register,
+        updateUserData
     }
-  };
 
-  const register = async (email: string, password: string, name: string) => {
-    try {
-      const res = await createUserWithEmailAndPassword(auth, email, password);
-
-      await setDoc(doc(firestore, "users", res.user.uid), {
-        name,
-        email,
-        uid: res.user.uid,
-        image: null, // or empty if you like
-      });
-
-      await updateUserData(res.user.uid);
-      return { success: true };
-    } catch (error: any) {
-      const msg = error?.message;
-      return { success: false, msg };
-    }
-  };
-
-  const updateUserData = async (uid: string) => {
-    try {
-      const docRef = doc(firestore, "users", uid);
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        const data = docSnap.data();
-
-        const userData: UserType = {
-          uid: data.uid,
-          email: data.email || null,
-          name: data.name || null,
-          image: data.image || null,
-        };
-        setUser(userData);
-      }
-    } catch (error) {
-      console.log("Error updating user data:", error);
-    }
-  };
-
-  const contextValue: AuthContextType ={
-    user,
-    setUser,
-    login,
-    register,
-    updateUserData
-  }
-
-  return (
-    <AuthContext.Provider value={contextValue}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 
-export const useAuth = ():AuthContextType => {
+export const useAuth = (): AuthContextType => {
     const context = useContext(AuthContext)
 
     if (!context) {
