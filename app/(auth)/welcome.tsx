3bb48b09d1f6ab7c8c4bd0b12bb36d1f24fad68b
preston176 <prestonnyamweya@gmail.com>
2025-04-26 10:56:22 +0300

    feat: add Button and Loading components for enhanced UI interaction in welcome screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -4,6 +4,8 @@ import ScreenWrapper from '@/components/ScreenWrapper'
 import Typo from '@/components/Typo'
 import { colors, spacingX, spacingY } from '@/constants/theme'
 import { verticalScale } from '@/utils/styling'
+import Button from '@/components/Button'
+import Animated, { FadeIn } from 'react-native-reanimated'
 
 const welcome = () => {
     return (
@@ -14,26 +16,33 @@ const welcome = () => {
                         <Typo fontWeight={"500"}>Sign In</Typo>
                     </TouchableOpacity>
 
-                    <Image
-                    source={require("../../assets/images/welcome.png")}
-                    style={styles.welcomeImage}
-                    resizeMode='contain'
+
+
+                    <Animated.Image
+                        entering={FadeIn.duration(500)}
+                        source={require("../../assets/images/welcome.png")}
+                        style={styles.welcomeImage}
+                        resizeMode='contain'
                     />
                 </View>
 
                 <View style={styles.footer}>
-                    <View style={{alignItems: 'center'}}>
+                    <View style={{ alignItems: 'center' }}>
                         <Typo size={30} fontWeight={"800"}>Take full control</Typo>
                         <Typo size={30} fontWeight={"800"}>of your finances</Typo>
 
                     </View>
-                    <View style={{alignItems: "center", gap: 2}}>
-                    <Typo size={17} color={colors.textLight}>Arrange your finances to set a better</Typo>
-                    <Typo size={17} color={colors.textLight}>lifestyle in future</Typo>
+                    <View style={{ alignItems: "center", gap: 2 }}>
+                        <Typo size={17} color={colors.textLight}>Arrange your finances to set a better</Typo>
+                        <Typo size={17} color={colors.textLight}>lifestyle in future</Typo>
                     </View>
 
                     <View style={styles.buttonContainer}>
-                        
+                        <Button>
+                            <Typo size={22} color={colors.neutral900} fontWeight={"600"}>
+                                Get Started
+                            </Typo>
+                        </Button>
                     </View>
                 </View>
             </View>
