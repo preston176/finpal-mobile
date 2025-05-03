27b1783895a6bac202bfcc9308d18b2c9862164d
preston176 <prestonnyamweya@gmail.com>
2025-05-03 10:32:51 +0300

    feat: Enhance statistics screen with segmented control and chart integration

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,14 +1,126 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { colors, radius, spacingX, spacingY } from '@/constants/theme'
+import { scale, verticalScale } from '@/utils/styling'
+import ScreenWrapper from '@/components/ScreenWrapper'
+import Header from '@/components/Header'
+import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated'
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
+import { BarChart, LineChart, PieChart, PopulationPyramid } from "react-native-gifted-charts"
+
+const barData = [
+  {
+    value: 40,
+    label: "Mon",
+    spacing: scale(4),
+    labelWidth: scale(30),
+    frontColor: colors.primary,
+  },
+  {
+    value: 20,
+    frontColor: colors.rose,
+  },
+  {
+    value: 50,
+    label: "Tue",
+    spacing: scale(4),
+    labelWidth: scale(30),
+    frontColor: colors.primary
+  },
+  { value: 40, frontColor: colors.rose }
+]
+
 
 const statistics = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const [chartdata, setChartData] = useState(barData)
+
+
+
   return (
-    <View>
-      <Text>statistics</Text>
-    </View>
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Header title='Statistics' />
+        </View>
+        <ScrollView contentContainerStyle={{
+          gap: spacingY._20,
+          paddingTop: spacingY._5,
+          paddingBottom: verticalScale(100)
+        }} showsVerticalScrollIndicator={false}>
+          <SegmentedControl
+            values={['Weekly', 'Monthly', "Yearly"]}
+            selectedIndex={activeIndex}
+            onChange={(event) => {
+              setActiveIndex(event.nativeEvent.selectedSegmentIndex)
+            }}
+            tintColor={colors.neutral200}
+            backgroundColor={colors.neutral800}
+            appearance='dark'
+            activeFontStyle={styles.segmentedFontStyle}
+            style={styles.segmentStyle}
+            fontStyle={{ ...styles.segmentedFontStyle, color: colors.white }}
+          />
+          <View style={styles.chartContainer}>
+            {
+              chartdata.length > 0 ? (
+                <BarChart barWidth={scale(12)} spacing={scale(25)} data={chartdata} roundedTop roundedBottom hideRules yAxisLabelPrefix='Kes' yAxisThickness={0} xAxisThickness={0} yAxisLabelWidth={scale(25)} yAxisTextStyle={{color: colors.neutral350}} xAxisLabelTextStyle={{color: colors.neutral350, fontSize: verticalScale(12)}}
+                noOfSections={3}
+                
+                />
+              ) : (
+                <View style={styles.noChart} />
+              )
+            }
+          </View>
+        </ScrollView>
+      </View >
+    </ScreenWrapper >
   )
 }
 
 export default statistics
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: spacingX._20,
+    paddingVertical: spacingY._5,
+    gap: spacingY._10
+
+  },
+  segmentedFontStyle: {
+    fontSize: verticalScale(13),
+    fontWeight: "bold",
+    color: colors.black
+  },
+  segmentStyle: {
+    height: scale(37),
+  },
+  searchIcon: {
+    backgroundColor: colors.neutral700,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    height: verticalScale(35),
+    width: verticalScale(35),
+    borderCurve: "continuous"
+  },
+  noChart: {
+    backgroundColor: "rgba(0,0,0, 0.6)",
+    height: verticalScale(210)
+  },
+  header: {},
+  chartLoadingContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    borderRadius: radius._12,
+    backgroundColor: "rgba(0,0,0, 0.6)"
+  },
+  chartContainer: {
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
